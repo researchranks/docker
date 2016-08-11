07a8db9a75619dc5aecbfb3ac0ad828fbe17b39c
researchranks <x@x-3.local>
2016-08-11 10:09:05 -0700

    updated readme @ 08/11/16 | 10:09:05 AM | August | 2016 | Thursday | (PDT)

---
+++ main.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+
+# Set magic variables for current file, directory, os, etc.
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename ${__file} .sh)"
+__github_repo="harden-ubuntu-server"
+__github_user="researchranks"
+
+# Debug setup
+# Exit on error. Append || true if you expect an error.
+# Do not allow use of undefined vars. Use ${VAR:-} for undefined
+# Catch error for mysqldump fail and gzip succeeds in `mysqldump |gzip`
+
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
+
+
+bash <(curl -s https://raw.githubusercontent.com/researchranks /harden-ubuntu-server/master/main.sh)
+
