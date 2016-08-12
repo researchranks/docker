6b6138dbc79aab1b2afed9015d2c2dfff291ca9c
researchranks <x@x-3.local>
2016-08-12 10:29:37 -0700

    updated readme @ 08/12/16 | 10:29:37 AM | August | 2016 | Friday | (PDT)

--- main.sh
+++ main.sh
@@ -52,13 +52,9 @@ update_ubuntu(){
 	apt-get autoclean -y
 }
 
-
-#install_firewall(){}
-
 install_docker(){
-	
 	apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-
+	
 	echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
 	
 	${update_ubuntu}
