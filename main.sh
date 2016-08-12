721805ef4561eab3a416b2e625b8e9df40978deb
researchranks <x@x-3.local>
2016-08-12 10:18:24 -0700

    updated readme @ 08/12/16 | 10:18:24 AM | August | 2016 | Friday | (PDT)

--- main.sh
+++ main.sh
@@ -14,6 +14,9 @@ __tmp_path="$(/bin/mktemp --directory -t)"
 __github_repository="docker"
 __github_username="researchranks"
 
+# Use {} when expanding a variable
+# Use () when executing a variable
+
 
 # Debug setup
 # Exit on error. Append || true if you expect an error.
@@ -29,15 +32,14 @@ set -o pipefail
 echo ${__dir}
 
 
-download_docker_repo(){
+install_setup(){
 	echo ${__tmp_path}
 	cd ${__tmp_path}
 	
 	git clone  https://github.com/${__github_username}/${__github_repository}.git
 	
 	mv ${__github_repository}/.profile ~/
-	source $HOME/.profile
-	rm -rf /tmp/${__tmp_path}
+	
 }
 
 
@@ -55,6 +57,23 @@ install_firewall(){
 	
 }
 
+install_docker(){
+	
+	apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
+	echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
+	
+	$(update_ubuntu)
+	
+	apt-cache policy docker-engine
+	
+	
+}
 
-download_docker_repo
+clean_up_install(){
+	source $HOME/.profile
+	rm -rf /tmp/${__tmp_path}
+}
+
+update_ubuntu
+install_setup
